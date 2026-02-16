cdf9977c0257b14f97fb4fe946713c69c74b0985
kanon6343 <kanon6343@gmail.com>
2026-02-16 22:49:46 +0900

    Update script.js

--- dev/script.js
+++ dev/script.js
@@ -1001,10 +1001,10 @@ function exportBackup() {
 
   // ✅ 復元直後は「板タブ含めて全部OFF」
   resetAllFiltersAndSort();  // ← 追加
-
+/*
  const resetUI = confirm(
     "フィルター/ソートを初期状態にリセットしますか？\n\nOK：UIをリセット\nキャンセル：バックアップのUIも復元"
-  ); 
+  ); 　*/
 
 
   // ✅ ここは「UIリセットしたい時だけ」にする
@@ -1032,11 +1032,11 @@ function exportBackup() {
   localStorage.setItem(KEY, JSON.stringify(pendingImport.items));
 
   resetAllFiltersAndSort();  // ← 追加
-
+/*
   const resetUI = confirm(
     "フィルター/ソートを初期状態にリセットしますか？\n\nOK：UIをリセット\nキャンセル：バックアップのUIも復元"
   ); 
-
+*/
  /* if (resetUI) {
     resetUIState();
   } else if (pendingImport.ui && typeof pendingImport.ui === "object") {
